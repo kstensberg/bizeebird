2e732afd369a5569b6d1a649ebabc304f0492a2a
Kevin Stensberg <kstensberg@users.noreply.github.com>
2016-04-09 10:08:27 -0700

    add db and models from old project

---
+++ bizeebird/Db/Model/Appointment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizeeBirdBoarding.Db.Model
+{
+    public class Appointment
+    {
+        [Key]
+        public int AppointmentId { get; set; }
+        public Customer Customer { get; set; }
+        public Bird Bird { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Status { get; set; }
+        public bool GroomingWings { get; set; }
+        public bool GroomingNails { get; set; }
+        public bool CageNeeded { get; set; }
+    }
+}
