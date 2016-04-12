b3ea3635a28ae7448d932b45a7dd406f2698fd59
Kevin Stensberg <kstensberg@gmail.com>
2016-04-11 19:22:37 -0700

    tweak solution for entity framework and sqlite

--- bizeebird/Ui/NewAppointmentDialog.cs
+++ bizeebird/Ui/NewAppointmentDialog.cs
@@ -1,4 +1,6 @@
 using System;
+using BizeeBirdBoarding.Db;
+using BizeeBirdBoarding.Db.Model;
 
 namespace BizeeBirdBoarding.Ui
 {
@@ -11,7 +13,19 @@ namespace BizeeBirdBoarding.Ui
 
 		protected void onOkButtonClicked (object sender, EventArgs e)
 		{
-			throw new NotImplementedException ();
+			using (var db = new BizeeBirdDbContext())
+			{
+				//TODO get from widgets
+				var customer = new Customer
+				{
+					Name = "Name",
+					BoardingRate = 0.0f,
+					Notes = "Notes"
+				};
+
+				db.Customers.Add(customer);
+				db.SaveChanges();
+			}
 		}
 
 		protected void onCancelButtonClicked (object sender, EventArgs e)
