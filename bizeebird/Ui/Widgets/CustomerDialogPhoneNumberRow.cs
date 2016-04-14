7064f8176c48db1f9a69889fd13c99dd5e24c72f
Kevin Stensberg <kstensberg@gmail.com>
2016-04-14 11:30:04 -0700

    hook up phone number buttons on customer dialog

---
+++ bizeebird/Ui/Widgets/CustomerDialogPhoneNumberRow.cs
@@ -0,0 +1,45 @@
+﻿using Gtk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bizeebird.Ui.Widgets
+{
+    class CustomerDialogPhoneNumberRow : Gtk.HBox
+    {
+        private Entry Entry;
+        private Button AddButton;
+        private Button RemoveButton;
+
+        public CustomerDialogPhoneNumberRow()
+        {
+            Entry = new Entry();
+            Add(Entry);
+
+			AddButton = new Button("Add");
+			Add(AddButton);
+
+			RemoveButton = new Button("Remove");
+            Add(RemoveButton);
+
+            ShowAll();
+        }
+
+        public void addOnAddButtonClicked(EventHandler handler)
+        {
+            AddButton.Clicked += handler;
+        }
+
+        public void addOnRemoveButtonClicked(EventHandler handler)
+        {
+            RemoveButton.Clicked += handler;
+        }
+
+        public string getPhoneNumber()
+        {
+            return Entry.Text;
+        }
+    }
+}
