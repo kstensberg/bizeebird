1170fc3b00548f0648c3b97dd3d7d2a83de561bc
Kevin Stensberg <kstensberg@users.noreply.github.com>
2016-04-16 20:13:31 -0700

    hook up customer treeview on main window

--- bizeebird/Ui/MainWindow.cs
+++ bizeebird/Ui/MainWindow.cs
@@ -1,16 +1,50 @@
+using BizeeBirdBoarding.Db;
+using BizeeBirdBoarding.Db.Model;
 using Gtk;
 using System;
 
 namespace BizeeBirdBoarding.Ui
 {
     public partial class MainWindow: Gtk.Window
-	{	
-		public MainWindow (): base (Gtk.WindowType.Toplevel)
-		{
-			Build ();
-		}
+	{
+        private Gtk.ListStore CustomersListStore;
 
-		protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+        public MainWindow() : base(Gtk.WindowType.Toplevel)
+        {
+            Build();
+
+            UiUtils.addColumnToTreeView(customersTreeview, "Name", 0, "text");
+            UiUtils.addColumnToTreeView(customersTreeview, "Phone Number", 1, "text");
+            UiUtils.addColumnToTreeView(customersTreeview, "E-mail Address", 2, "text");
+            UiUtils.addColumnToTreeView(customersTreeview, "Boarding Rate", 3, "text");
+            UiUtils.addColumnToTreeView(customersTreeview, "Notes", 4, "text");
+
+            CustomersListStore = new Gtk.ListStore(typeof(string), typeof(string), typeof(string), typeof(double), typeof(string));
+
+            customersTreeview.Model = CustomersListStore;
+
+            updateCustomerList();
+        }
+
+        private void updateCustomerList()
+        {
+            CustomersListStore.Clear();
+
+            using (var db = new BizeeBirdDbContext())
+            {
+                foreach (Customer row in db.Customers)
+                {
+                    string phoneNumber = "";
+
+                    if (row.PhoneNumbers != null && row.PhoneNumbers.Count > 0)
+                        phoneNumber = row.PhoneNumbers[0].PhoneNumber;
+
+                    CustomersListStore.AppendValues(row.Name, phoneNumber, row.Email, row.BoardingRate, row.Notes);
+                }
+            }
+        }
+
+        protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 		{
 			Application.Quit ();
 			a.RetVal = true;
@@ -19,7 +53,14 @@ namespace BizeeBirdBoarding.Ui
 		protected void onNewCustomerClicked (object sender, EventArgs e)
 		{
 			NewCustomerDialog dialog = new NewCustomerDialog ();
-			dialog.ShowAll ();
+
+            dialog.Destroyed += delegate
+            {
+                updateCustomerList();
+            };
+
+
+            dialog.ShowAll ();
 		}
 
 		protected void onNewApointmentButtonClicked (object sender, EventArgs e)
