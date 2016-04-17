f3a18409199d566d8657bae965c3e98688f60d97
Kevin Stensberg <kstensberg@users.noreply.github.com>
2016-04-17 02:39:22 -0700

    hook up customer and bird combo on appointment dialog

--- bizeebird/Ui/NewAppointmentDialog.cs
+++ bizeebird/Ui/NewAppointmentDialog.cs
@@ -1,3 +1,6 @@
+using BizeeBirdBoarding.Db;
+using BizeeBirdBoarding.Db.Model;
+using Gtk;
 using System;
 
 namespace BizeeBirdBoarding.Ui
@@ -6,18 +9,61 @@ namespace BizeeBirdBoarding.Ui
 	{
 		public NewAppointmentDialog ()
 		{
-			this.Build ();
-		}
+			this.Build();
 
-		protected void onOkButtonClicked (object sender, EventArgs e)
+            customerCombobox.Clear();
+            CellRendererText cell = new CellRendererText();
+            customerCombobox.PackStart(cell, false);
+            customerCombobox.AddAttribute(cell, "text", 0);
+            ListStore store = new ListStore(typeof(string), typeof(int));
+            customerCombobox.Model = store;
+
+            using (var db = new BizeeBirdDbContext())
+            {
+                foreach (Customer row in db.Customers)
+                {
+                    store.AppendValues(row.Name, row.CustomerId);
+                }
+            }
+        }
+
+		protected void onOkButtonClicked(object sender, EventArgs e)
 		{
             throw new NotImplementedException();
         }
 
-		protected void onCancelButtonClicked (object sender, EventArgs e)
+		protected void onCancelButtonClicked(object sender, EventArgs e)
 		{
             Destroy();
 		}
-	}
+
+        protected void onCustomerComboChanged(object sender, EventArgs e)
+        {
+            TreeIter iter;
+            if (customerCombobox.GetActiveIter(out iter))
+            {
+                int customerId = (int)customerCombobox.Model.GetValue(iter, 1);
+                Console.WriteLine();
+
+                birdCombobox.Clear();
+                CellRendererText cell = new CellRendererText();
+                birdCombobox.PackStart(cell, false);
+                birdCombobox.AddAttribute(cell, "text", 0);
+                ListStore store = new ListStore(typeof(string), typeof(int));
+                birdCombobox.Model = store;
+
+                using (var db = new BizeeBirdDbContext())
+                {
+                    Customer customer = db.Customers.Find(customerId);
+
+                    foreach (Bird row in customer.Birds)
+                    {
+                        store.AppendValues(row.Name, row.BirdId);
+                    }
+                }
+            }
+        }
+
+    }
 }
 
