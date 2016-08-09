ba672ac70e4af9a07b4ce006fb3eea8c12c0a3ad
Kevin Stensberg <kstensberg@gmail.com>
2016-08-08 22:11:26 -0700

    Added tag 1.2 for changeset 1b6916538cf8

--- bizeebird/BizeeBird.cs
+++ bizeebird/BizeeBird.cs
@@ -34,7 +34,7 @@ namespace BizeeBirdBoarding
             try
             {
                 WebRequest webRequest = WebRequest.Create("http://erza.net/bizeebird/update");
-                webRequest.Headers.Add("Version", "1.1");
+                webRequest.Headers.Add("Version", "1.2");
                 HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
 
                 if (response.StatusCode == HttpStatusCode.OK)
