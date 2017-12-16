d41abffea13acf1723d36b21f9e1704f3735bacb
Kevin Stensberg <kstensberg@gmail.com>
2017-12-15 20:13:36 -0800

    #4 fix some remaining bugs with DateMultiSelect

--- bizeebird/Ui/Widgets/DateMultiSelect.cs
+++ bizeebird/Ui/Widgets/DateMultiSelect.cs
@@ -1,4 +1,6 @@
-﻿using Gtk;
+﻿using BizeeBirdBoarding.Db;
+using BizeeBirdBoarding.Db.Model;
+using Gtk;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -8,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace BizeeBirdBoarding.Ui.Widgets
 {
-    class DateMultiSelect : Gtk.VBox
+    class DateMultiSelect : Gtk.HBox
     {
         public DateTime Date
         {
@@ -26,15 +28,13 @@ namespace BizeeBirdBoarding.Ui.Widgets
         {
             DateTime now = DateTime.Now;
 
-            MonthCombo = new ComboBox(new string[] { "January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" });
+            MonthCombo = new ComboBox();
             DayCombo = new ComboBox();
             YearCombo = new ComboBox();
 
-            HBox comboHbox = new HBox();
-            comboHbox.Add(MonthCombo);
-            comboHbox.Add(DayCombo);
-            comboHbox.Add(YearCombo);
-            Add(comboHbox);
+            this.Add(MonthCombo);
+            this.Add(DayCombo);
+            this.Add(YearCombo);
 
             SetDate(DateTime.Now);
 
@@ -50,45 +50,60 @@ namespace BizeeBirdBoarding.Ui.Widgets
             _Date = dateTime;
 
             PopulateDropDowns();
+
+            SetComboActive(MonthCombo, _Date.Month);
+            SetComboActive(DayCombo, _Date.Day);
+            SetComboActive(YearCombo, _Date.Year);
         }
 
-        private void PopulateDropDowns()
-        {          
+        public void PopulateDropDowns()
+        {
+            UpdateCombo(MonthCombo, new Dictionary<int, string>()
+            {
+                { 1, "January"},
+                { 2, "Febuary"},
+                { 3, "March"},
+                { 4, "April"},
+                { 5, "May"},
+                { 6, "June"},
+                { 7, "July"},
+                { 8, "August"},
+                { 9, "September"},
+                { 10, "October"},
+                { 11, "November"},
+                { 12, "December"}
+            });
+
             int daysInMonth = DateTime.DaysInMonth(_Date.Year, _Date.Month);
-
-            var years = Enumerable.Range(_Date.Year - 5, 11).Select(x => x.ToString()).ToList();
-            var days = Enumerable.Range(1, daysInMonth).Select(x => x.ToString()).ToList();
-
-            UpdateCombo(YearCombo, years);
+            var days = Enumerable.Range(1, daysInMonth).ToDictionary(v => v, v => v.ToString());
             UpdateCombo(DayCombo, days);
 
-            SetComboActive(YearCombo, _Date.Year.ToString());
-            SetComboActive(DayCombo, _Date.Day.ToString());
-
-            var monthStr = new DateTimeFormatInfo().GetMonthName(_Date.Month);
-
-            SetComboActive(MonthCombo, monthStr);
+            var years = Enumerable.Range(_Date.Year - 5, 11).ToDictionary(v => v, v => v.ToString());
+            UpdateCombo(YearCombo, years);
         }
 
-        private void UpdateCombo(ComboBox comboBox, List<string> values)
+        private void UpdateCombo(ComboBox comboBox, Dictionary<int, string> values)
         {
-            comboBox.Changed -= OnComboChanged;
             comboBox.Clear();
             CellRendererText cell = new CellRendererText();
             comboBox.PackStart(cell, false);
             comboBox.AddAttribute(cell, "text", 0);
-            ListStore store = new ListStore(typeof(string));
+            ListStore store = new ListStore(typeof(string), typeof(int));
             comboBox.Model = store;
 
-            foreach (var value in values)
+            using (var db = new BizeeBirdDbContext())
             {
-                store.AppendValues(value);
-            }
 
-            comboBox.Changed += OnComboChanged;
+                IQueryable set = db.Customers;
+
+                foreach (KeyValuePair<int, string> kvp in values)
+                {
+                    store.AppendValues(kvp.Value, kvp.Key);
+                }
+            }
         }
 
-        private void SetComboActive(ComboBox comboBox, string activeValue)
+        private void SetComboActive(ComboBox comboBox, int activeValue)
         {
             Gtk.TreeIter iter;
 
@@ -96,13 +111,11 @@ namespace BizeeBirdBoarding.Ui.Widgets
             do
             {
                 GLib.Value thisRow = new GLib.Value();
-                comboBox.Model.GetValue(iter, 0, ref thisRow);
+                comboBox.Model.GetValue(iter, 1, ref thisRow);
 
-                if (thisRow.Val.ToString() == activeValue)
+                if ((int)thisRow.Val == activeValue)
                 {
-                    comboBox.Changed -= OnComboChanged;
                     comboBox.SetActiveIter(iter);
-                    comboBox.Changed += OnComboChanged;
                     return;
                 }
             } while (comboBox.Model.IterNext(ref iter));
@@ -110,60 +123,39 @@ namespace BizeeBirdBoarding.Ui.Widgets
 
         private void OnComboChanged(object sender, EventArgs args)
         {
-            var month = GetComboValue(MonthCombo);
-            var day = GetComboValue(DayCombo);
-            var year = GetComboValue(YearCombo);
+            int month = GetComboValue(MonthCombo);
+            int day = GetComboValue(DayCombo);
+            int year = GetComboValue(YearCombo);
 
-            if (month == null || day == null || year == null)
+            if (month == -1 || day == -1 || year == -1)
                 return;
 
-            int monthInt = GetNumericMonth(month);
-            int dayInt = Int32.Parse(day);
-            int yearInt = Int32.Parse(year);
-
-            if (month != new DateTimeFormatInfo().GetMonthName(_Date.Month) ||
-                dayInt != _Date.Day ||
-                yearInt != _Date.Year)
+            if (month != _Date.Month ||
+                day != _Date.Day ||
+                year != _Date.Year)
             {
-                DateTime newDate = new DateTime(yearInt, monthInt, dayInt);
+                if (day > DateTime.DaysInMonth(year, month))
+                    day = 1;
+
+                DateTime newDate = new DateTime(year, month, day);
                 SetDate(newDate);
             }
         }
 
-        private string GetComboValue(ComboBox comboBox)
+        private int GetComboValue(ComboBox comboBox)
         {
             Gtk.TreeIter iter;
             comboBox.GetActiveIter(out iter);
 
             GLib.Value activeRow = new GLib.Value();
-            comboBox.Model.GetValue(iter, 0, ref activeRow);
+            comboBox.Model.GetValue(iter, 1, ref activeRow);
 
             if (activeRow.Val == null)
             {
-                return null;
+                return -1;
             }
 
-            return activeRow.Val.ToString();
-        }
-
-        private int GetNumericMonth(string month)
-        {
-            switch (month)
-            {
-                case "January": return 1;
-                case "Febuary": return 2;
-                case "March": return 3;
-                case "April": return 4;
-                case "May": return 5;
-                case "June": return 6;
-                case "July": return 7;
-                case "August":return 8;
-                case "September": return 9;
-                case "October": return 10;
-                case "November": return 11;
-                case "December": return 12;
-            }
-            return 0;
+            return (int)activeRow.Val;
         }
     }
 }
