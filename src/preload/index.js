1a6e32c5fd551340eb6c09127e16776acf9aed23
Kevin Stensberg <kstensberg@gmail.com>
2022-08-18 03:01:43 -0700

    add buttons to main window

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,14 +1,5 @@
-'use strict';
+const{ contextBridge, ipcRenderer }=require('electron')
 
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-        const element = document.getElementById(selector);
-        if (element) {
-            element.innerText = text;
-        }
-    };
-
-    for (const dependency of ['chrome', 'node', 'electron']) {
-        replaceText(`${dependency}-version`, process.versions[dependency]);
-    }
-});
+contextBridge.exposeInMainWorld('contextBridge',{
+openWindow:(url)=> ipcRenderer.send('open-window', url)
+})
