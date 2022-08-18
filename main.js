7e06d156428d26bfebf2b1504bf37988b85423f0
Kevin Stensberg <kstensberg@gmail.com>
2022-08-18 03:01:43 -0700

    add buttons to main window

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 'use strict';
 
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+
+//ipc
+ipcMain.on('open-window', (event, path) => {
+    const newWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        autoHideMenuBar: true
+    });
+
+    newWindow.loadFile(path);
+});
 
 const createWindow = () => {
     const mainWindow = new BrowserWindow({
