836a9563bff268ff43dd8de6d6f3680bf7ee9115
Kevin Stensberg <kstensberg@gmail.com>
2022-08-21 19:35:34 -0700

    add upcoming pickups table

---
+++ src/renderer/components/upcoming-pickups-table.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import { Table } from './table.js';
+
+var UpcomingPickupsTable = {
+    dataRows: [],
+    oninit: async function(vnode) {
+        const response = await window.contextBridge.database.getUpcomingPickups();
+
+        for (const row of response) {
+            this.dataRows.push([row.Date, row.Name, row.BirdName, row.BirdBreed, row.Wings, row.Nails, row.Rate, row.Notes]);
+        }
+
+        m.redraw();
+    },
+    view: function(vnode) {
+        return m(Table, {
+            headers: ['Date', 'Customers', 'Bird Name', 'Bird Breed', 'Wings', 'Nails', 'Rate', 'Notes'],
+            data: this.dataRows
+        });
+
+        
+    }
+};
+
+export { UpcomingPickupsTable };
