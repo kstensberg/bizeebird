e471856e55e25fe9f6570179f613421e3f4d4d76
Kevin Stensberg <kstensberg@gmail.com>
2022-08-21 22:34:06 -0700

    customer search tab

--- src/preload/index.js
+++ src/preload/index.js
@@ -4,6 +4,7 @@ contextBridge.exposeInMainWorld('contextBridge',{
     openWindow:(url)=> ipcRenderer.send('open-window', url),
     database: {
         getAllCustomers: () => ipcRenderer.invoke('getAllCustomers'),
+        searchCustomers: (searchString) => ipcRenderer.invoke('searchCustomers', searchString),
         getUpcomingDropoffs: () => ipcRenderer.invoke('getUpcomingDropoffs'),
         getUpcomingPickups: () => ipcRenderer.invoke('getUpcomingPickups'),
     }
---
+++ src/renderer/components/customers-search.js
@@ -0,0 +1,50 @@
+'use strict';
+
+import { Table } from './table.js';
+
+var CustomerSearch = {
+    dataRows: [],
+    updateTable: async function(searchString) {
+        this.dataRows = [];
+        let data = [];
+        if (searchString.length == 0) {
+            data = await window.contextBridge.database.getAllCustomers();
+        } else {
+            data = await window.contextBridge.database.searchCustomers(searchString);
+        }
+
+        for (const row of data) {
+            this.dataRows.push([row.Name, row.PhoneNumber, row.Email, '$' + row.BoardingRate.toFixed(2), row.Notes]);
+        }
+
+        m.redraw();
+    },
+    oninit: async function(vnode) {
+        await this.updateTable('');
+    },
+    view: function(vnode) {
+        const component = this;
+        return m('div', [
+            m('div', { 'class':'form-floating mb-3' }, [
+                m('input', {
+                    'class':'form-control',
+                    'type':'text',
+                    'placeholder':'name@example.com',
+                    onkeyup: function(e) {
+                        component.updateTable(e.target.value);
+                    }
+                }),
+                m('label', { 'for':'floatingInput' },
+                    'Customer Search'
+                )]),
+            m(Table, {
+                headers: ['Customer Name', 'Phone Number', 'Email', 'Boarding Rate', 'Notes'],
+                data: this.dataRows
+            }),
+        ]);
+
+
+    }
+};
+
+export { CustomerSearch };
