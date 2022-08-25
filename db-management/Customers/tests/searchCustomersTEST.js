56f050816c66671e2bb75e7e284e9904c643ba05
Shawn Stensberg <shawn.stensberg@gmail.com>
2022-08-25 16:49:57 -0700

    updated searchCustomerTEST assertions and removed unnecessary if statements

--- db-management/Customers/tests/searchCustomersTEST.js
+++ db-management/Customers/tests/searchCustomersTEST.js
@@ -4,22 +4,34 @@ const searchCustomers = require('../queries/searchCustomers');
 
 describe('#searchCustomers()', function () {
     it('should pass if a single customer was located by email address', async () => {
-        const customer = await searchCustomers('helenacallis@gmail.com');
-        assert.equal(Object.keys(customer).length, 1);
+        const customer = await searchCustomers('j.spunk@hotmail.com');
+        assert.typeOf(customer, 'array', 'Customer is an array');
+        assert.equal(customer.length, 1);
+        assert.equal(customer[0].Email, 'j.spunk@hotmail.com');
     });
 
     it('should pass if a single customer was located by exact name', async () => {
         const customer = await searchCustomers('Wendy Hinsberger');
-        assert.equal(Object.keys(customer).length, 1);
-    })
+        assert.typeOf(customer, 'array', 'Customer is an array');
+        assert.equal(customer.length, 1);
+        assert.equal(customer[0].Name, 'Wendy Hinsberger');
+    });
 
     it('should pass if more than one customer was located by email address', async () => {
         const customer = await searchCustomers('helenacallis@gmail.com');
-        assert.isAbove(Object.keys(customer).length, 1);
+        assert.typeOf(customer, 'array', 'Customer is an array');
+        assert.isAbove(customer.length, 1);
+        customer.forEach(item => {
+            assert.equal(item.Email,'helenacallis@gmail.com');
+        });
     });
 
     it('should pass if more than one customer was located by exact name', async () => {
         const customer = await searchCustomers('zzz unknown');
-        assert.isAbove(Object.keys(customer).length, 1);
+        assert.typeOf(customer, 'array', 'Customer is an array');
+        assert.isAbove(customer.length, 1);
+        customer.forEach(item => {
+            assert.equal(item.Name,'zzz unknown');
+        });
     });
 });
