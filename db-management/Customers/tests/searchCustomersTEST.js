92389d42a5736c5014e530c3961854940c6a68ce
Shawn Stensberg <shawn.stensberg@gmail.com>
2022-08-25 14:56:29 -0700

    cleaned up searchCustomersTEST - removed unnecessary if statements

--- db-management/Customers/tests/searchCustomersTEST.js
+++ db-management/Customers/tests/searchCustomersTEST.js
@@ -1,54 +1,25 @@
-var assert = require('assert');
+var chai = require('chai'),
+    assert = chai.assert;
 const searchCustomers = require('../queries/searchCustomers');
 
-let actual;
-let expected;
-describe('#customerSearch()', function () {
+describe('#searchCustomers()', function () {
     it('should pass if a single customer was located by email address', async () => {
         const customer = await searchCustomers('helenacallis@gmail.com');
-        if (customer.length == 1) {
-            actual = true;
-            expected = true;
-        } else {
-            actual = false;
-            expected = true;
-        }
-        assert.equal(actual, expected);
+        assert.equal(Object.keys(customer).length, 1);
     });
 
     it('should pass if a single customer was located by exact name', async () => {
         const customer = await searchCustomers('Wendy Hinsberger');
-        if (customer.length == 1) {
-            actual = true;
-            expected = true;
-        } else {
-            actual = false;
-            expected = true;
-        }
-        assert.equal(actual, expected);
+        assert.equal(Object.keys(customer).length, 1);
     })
 
     it('should pass if more than one customer was located by email address', async () => {
         const customer = await searchCustomers('helenacallis@gmail.com');
-        if (customer.length > 1) {
-            actual = true;
-            expected = true;
-        } else {
-            actual = false;
-            expected = true;
-        }
-        assert.equal(actual, expected);
+        assert.isAbove(Object.keys(customer).length, 1);
     });
 
     it('should pass if more than one customer was located by name', async () => {
         const customer = await searchCustomers('zzz unknown');
-        if (customer.length > 1) {
-            actual = true;
-            expected = true;
-        } else {
-            actual = false;
-            expected = true;
-        }
-        assert.equal(actual, expected);
+        assert.isAbove(Object.keys(customer).length, 1);
     });
 });
