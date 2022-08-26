707737f3d49e1d60578370c9faaa285866b05f39
Shawn Stensberg <shawn.stensberg@gmail.com>
2022-08-25 18:05:52 -0700

    searchCustomers method updated to include email or name filter on same line

--- db-management/Customers/queries/searchCustomers.js
+++ db-management/Customers/queries/searchCustomers.js
@@ -4,12 +4,8 @@ const searchCustomers = (searchString) => {
     return new Promise((resolve, reject) => {
         db.serialize(() => {
             searchString = '%' + searchString + '%';
-            if (searchString.includes('@')) {
-                var query = 'SELECT Name, Email, BoardingRate, Notes FROM Customers WHERE Email LIKE ?';
-            } else {
-                query = 'SELECT Name, Email, BoardingRate, Notes FROM Customers WHERE Name LIKE ?';
-            }
-            db.all(query, searchString, (err, row) => {
+            var query = 'SELECT Name, Email, BoardingRate, Notes FROM Customers WHERE Email LIKE ? OR Name LIKE ?';
+            db.all(query, searchString, searchString, (err, row) => {
                 if (err) {
                     reject(err);
                 }
