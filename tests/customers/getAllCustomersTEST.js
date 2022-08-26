16e15e2780e6df44410ac2bcd82790e735bb654c
Shawn Stensberg <shawn.stensberg@gmail.com>
2022-08-25 18:20:06 -0700

    removed noise from console logs on getAllCustomersTEST

--- tests/customers/getAllCustomersTEST.js
+++ tests/customers/getAllCustomersTEST.js
@@ -1,14 +1,11 @@
-var assert = require('assert');
+var chai = require('chai'),
+    assert = chai.assert;
 const getAllCustomers = require('../../db-management/Customers/queries/getAllCustomers.js');
 
 describe('#getAllCustomers()', function () {
-    it('should return a list of all customers', function () {
-        const customers = getAllCustomers();
-        customers.then(function(result) {
-            console.log(result);
-        }).catch((error) => {
-            console.error(error);
-        });
+    it('should return a list of all customers', async function () {
+        const customers = await getAllCustomers();
+        assert.isAbove(customers.length, 10);
     });
 });
 
