7e844239bfe4608ed7cda3195358dcce253f4a2c
Shawn Stensberg <shawn.stensberg@gmail.com>
2022-08-29 10:27:11 -0700

    resolves #40 updated DB location to use app data

--- db-management/dbConfig.js
+++ db-management/dbConfig.js
@@ -1,4 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./db-management/bizeebird-old.db');
+const dbLocation = process.env.APPDATA;
+const db = new sqlite3.Database(dbLocation + '/BiZeeBird/bizeebird.db');
 
 module.exports = db;
