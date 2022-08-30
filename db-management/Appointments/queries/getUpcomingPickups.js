9457d16437a252f8ec518d7f67da127f3041ab36
Shawn Stensberg <shawn.stensberg@gmail.com>
2022-08-30 11:30:51 -0700

    created method for getting upcoming pick ups and connected to front-end

--- db-management/Appointments/queries/getUpcomingPickups.js
+++ db-management/Appointments/queries/getUpcomingPickups.js
@@ -4,12 +4,13 @@ const getUpcomingDropoffs = () => {
     return new Promise((resolve, reject) => {
         db.serialize(() => {
             var query = 'SELECT Customers.CustomerId, Customers.Name, Customers.BoardingRate AS Rate, Birds.Name AS BirdName,' +
-            'Birds.Breed, AppointmentBirds.CageNeeded, Appointments.StartTime AS Date FROM Customers ' +
+            'Birds.Breed, AppointmentBirds.GroomingWings AS Wings, AppointmentBirds.GroomingNails AS Nails, ' +
+            'Appointments.Notes AS Notes, Appointments.EndTime AS Date FROM Customers ' +
             'LEFT JOIN Birds ON Birds.Customer_CustomerId = Customers.CustomerId ' +
             'LEFT JOIN Appointments ON Appointments.Customer_CustomerId = Customers.CustomerId ' +
             'LEFT JOIN AppointmentBirds ON AppointmentBirds.Appointment_AppointmentId = Appointments.AppointmentId ' +
-            'WHERE Appointments.StartTime > date("now", "-30 days") AND Appointments.Status = 1 ' +
-            'GROUP BY CustomerId ORDER BY Appointments.EndTime DESC';
+            'WHERE Appointments.EndTime > date("now", "-30 days") AND Appointments.Status = 2 ' +
+            'GROUP BY CustomerId ORDER BY Appointments.StartTime DESC';
             db.all(query, (err, row) => {
                 if (err) {
                     reject(err);
