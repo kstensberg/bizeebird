efda2e7b8e64459c0b412db7b2eade7bf1050273
Shawn Stensberg <shawn.stensberg@gmail.com>
2022-08-30 19:21:02 -0700

    resolving some PR change requests

--- db-management/Appointments/queries/getUpcomingDropoffs.js
+++ db-management/Appointments/queries/getUpcomingDropoffs.js
@@ -4,17 +4,18 @@ const getUpcomingDropoffs = () => {
     return new Promise((resolve, reject) => {
         db.serialize(() => {
             var query = 'SELECT Customers.CustomerId, Customers.Name, Customers.BoardingRate AS Rate, Birds.Name AS BirdName,' +
-            'Birds.Breed, AppointmentBirds.CageNeeded, Appointments.StartTime AS Date FROM Customers ' +
+            'Birds.Breed, AppointmentBirds.GroomingWings AS Wings, AppointmentBirds.GroomingNails AS Nails, ' +
+            'Appointments.Notes AS Notes, Appointments.StartTime AS Date FROM Customers ' +
             'LEFT JOIN Birds ON Birds.Customer_CustomerId = Customers.CustomerId ' +
             'LEFT JOIN Appointments ON Appointments.Customer_CustomerId = Customers.CustomerId ' +
             'LEFT JOIN AppointmentBirds ON AppointmentBirds.Appointment_AppointmentId = Appointments.AppointmentId ' +
-            'WHERE Appointments.StartTime > date("now", "-30 days") AND Appointments.Status = 1 ' +
-            'GROUP BY CustomerId ORDER BY Appointments.EndTime DESC';
-            db.all(query, (err, row) => {
+            'WHERE Appointments.StartTime >= date("now") AND Appointments.Status = 0 ' +
+            'GROUP BY Appointments.AppointmentId ORDER BY Appointments.StartTime ASC';
+            db.all(query, (err, rows) => {
                 if (err) {
                     reject(err);
                 }
-                resolve(row);
+                resolve(rows);
             });
         });
     });
