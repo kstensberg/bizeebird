06e549af41ebf0f42ff80c3da7e3a964ce967e56
Shawn Stensberg <shawn.stensberg@gmail.com>
2022-09-02 16:51:06 -0700

    added method for getting single bird based on birdId

---
+++ db-management/Birds/queries/getSingleBird.js
@@ -0,0 +1,14 @@
+const getSingleBird = (db, birdId) => {
+    return new Promise((resolve, reject) => {
+        db.serialize(() => {
+            db.get('SELECT Deleted, Name, Breed, Color, Age, Gender, Notes FROM Birds WHERE BirdId = ?', birdId, (err, row) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(row);
+            });
+        });
+    });
+};
+
+module.exports = getSingleBird;
