4fdca0a538b95097a2812cb8edc612b3f5375ea9
Shawn Stensberg <shawn.stensberg@gmail.com>
2022-09-03 14:32:21 -0700

    cleaned up some queries for better readability

--- db-management/Birds/queries/getAllBirdsForSingleCustomer.js
+++ db-management/Birds/queries/getAllBirdsForSingleCustomer.js
@@ -1,7 +1,8 @@
 const getAllBirdsForSingleCustomer = (db, customerID) => {
     return new Promise((resolve, reject) => {
         db.serialize(() => {
-            db.all('SELECT BirdId, Deleted, Name, Breed, Color, Age, Gender, Notes FROM Birds WHERE Customer_CustomerId = ?', customerID, (err, row) => {
+            db.all('SELECT BirdId, Deleted, Name, Breed, Color, Age, Gender, ' +
+            'Notes FROM Birds WHERE Customer_CustomerId = ?', customerID, (err, row) => {
                 if (err) {
                     reject(err);
                 }
--- db-management/Customers/queries/searchCustomers.js
+++ db-management/Customers/queries/searchCustomers.js
@@ -4,7 +4,11 @@ const searchCustomers = (db, searchString) => {
     return new Promise((resolve, reject) => {
         db.serialize(() => {
             searchString = '%' + searchString + '%';
-            var query = 'SELECT Customers.Name, CustomerPhoneNumbers.PhoneNumber AS PhoneNumber, Customers.Email, Customers.BoardingRate, Customers.Notes FROM Customers LEFT JOIN CustomerPhoneNumbers ON CustomerPhoneNumbers.Customer_CustomerId = Customers.CustomerId WHERE Customers.Email LIKE ? OR Customers.Name LIKE ? OR CustomerPhoneNumbers.PhoneNumber LIKE ? GROUP BY Customers.Name';
+            var query = 'SELECT Customers.Name, CustomerPhoneNumbers.PhoneNumber AS PhoneNumber, ' +
+            'Customers.Email, Customers.BoardingRate, Customers.Notes FROM Customers ' +
+            'LEFT JOIN CustomerPhoneNumbers ON CustomerPhoneNumbers.Customer_CustomerId = Customers.CustomerId ' +
+            'WHERE Customers.Email LIKE ? OR Customers.Name LIKE ? OR CustomerPhoneNumbers.PhoneNumber LIKE ? ' +
+            'GROUP BY Customers.Name';
             db.all(query, searchString, searchString, searchString, (err, row) => {
                 if (err) {
                     reject(err);
