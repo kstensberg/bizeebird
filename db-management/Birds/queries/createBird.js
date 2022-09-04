2207ea930747ad134a3b8d1a67e51d17566a34ed
Shawn Stensberg <shawn.stensberg@gmail.com>
2022-09-03 19:51:32 -0700

    adjusted createBird.js method to take in bird object

--- db-management/Birds/queries/createBird.js
+++ db-management/Birds/queries/createBird.js
@@ -1,15 +1,15 @@
-const createBird = (db, Name, Breed, Color, Age, Gender, Notes, CustomerId) => {
+const createBird = (db, Bird) => {
     db.serialize(() => {
         db.run('INSERT INTO Birds (Deleted, Name, Breed, Color, Age, Gender, Notes, ' +
         'Customer_CustomerId) VALUES (Deleted = 0, Name = $Name, Breed = $Breed, Color = $Color, Age = $Age, ' +
         'Gender = $Gender, Notes = $Notes, Customer_CustomerId = $CustomerId)', {
-            $Name: Name,
-            $Breed: Breed,
-            $Color: Color,
-            $Age: Age,
-            $Gender: Gender,
-            $Notes: Notes,
-            $CustomerId: CustomerId
+            $Name: Bird.Name,
+            $Breed: Bird.Breed,
+            $Color: Bird.Color,
+            $Age: Bird.Color,
+            $Gender: Bird.Gender,
+            $Notes: Bird.Notes,
+            $Customer_CustomerId: Bird.CustomerId
         });
     });
 };
