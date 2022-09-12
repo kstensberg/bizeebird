2938281e75a3b85081876de4dfd17f6f3e6cc633
Shawn Stensberg <shawn.stensberg@gmail.com>
2022-09-12 16:56:56 -0700

    removed files from commit

--- src/db-management/Create/createAllAppointment.js
+++ src/db-management/Create/createAllAppointment.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const createAppointment = (db, customer, appointment) => {
+const createAppointment = (db, appointment) => {
     return new Promise((resolve, reject) => {
         db.serialize(() => {
             db.run('INSERT INTO Appointments (StartTime, EndTime, Status, Notes, Customer_CustomerId) ' +
             'VALUES ($StartTime, $EndTime, $Status, $Notes, $Customer_CustomerId)'), {
-                $StartTime: appointment.startTime,
-                $EndTime: appointment.endTime,
-                $Status: appointment.appointmentStatus,
+                $StartTime: appointment.startDate,
+                $EndTime: appointment.endDate,
+                $Status: appointment.status,
                 $Notes: appointment.notes,
-                $Customer_CustomerId: customer.customerId
+                $Customer_CustomerId: appointment.customerId
             }, function(err) {
                 if (err) {
                     reject(err);
@@ -22,7 +22,7 @@ const createAppointment = (db, customer, appointment) => {
 
 const createAppointmentBirds = (db, appointmentId, appointmentBird, birds) => {
     db.serialize(() => {
-        for (const bird of birds) {
+        birds.forEach(bird =>
             db.run('INSERT INTO AppointmentBirds (GroomingWings, GroomingNails, CageNeeded, ' +
             'Bird_BirdId, Appointment_AppointmentId, ApptBirdNotes) VALUES ($GroomingWings, ' +
             '$GroomingNails, $CageNeeded, $Bird_BirdId, ' +
@@ -30,17 +30,20 @@ const createAppointmentBirds = (db, appointmentId, appointmentBird, birds) => {
                 $GroomingWings: appointmentBird.groomingWings,
                 $GroomingNails: appointmentBird.groomingNails,
                 $CageNeeded: appointmentBird.cageNeeded,
-                $Bird_BirdId: bird.birdId,
+                $Bird_BirdId: bird,
                 $Appointment_AppointmentId: appointmentId,
                 $ApptBirdNotes: appointmentBird.notes
-            });
-        }
+            })
+        );
     });
 };
 
-const runAllCreateAppointment = async (db, customer, appointment, appointmentBird) => {
-    const appointmentId = await createAppointment(db, customer, appointment);
-    await Promise.all([createAppointmentBirds(db, appointmentId, appointmentBird, customer.birds)]);
+const runAllCreateAppointment = async (db, appointment) => {
+    console.log(appointment);
+    const appointmentId = await createAppointment(db, appointment);
+    console.log(appointmentId);
+    await Promise.all([createAppointmentBirds(db, appointmentId, appointment.birds)]);
 };
 
 module.exports = runAllCreateAppointment;
+
