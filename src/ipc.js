f462edd0cc3fce45943567bf4a76bdabc957bc16
Shawn Stensberg <shawn.stensberg@gmail.com>
2022-09-12 11:41:14 -0700

    removed root directories .vscode and db-management

--- src/ipc.js
+++ src/ipc.js
@@ -13,37 +13,37 @@ const { BrowserWindow, ipcMain } = require('electron');
 const db = require('./db-management/dbConfig');
 
 ipcMain.handle('getAllCustomers', async function(event) {
-    const getAllCustomers = require('./db-management/Customers/queries/getAllCustomers');
+    const getAllCustomers = require('./db-management/Get/getAllCustomers');
     return getAllCustomers(db);
 });
 
 ipcMain.handle('searchCustomers', async function(event, searchString) {
-    const searchCustomers = require('./db-management/Customers/queries/searchCustomers');
+    const searchCustomers = require('./db-management/Search/searchCustomers');
     return searchCustomers(db, searchString);
 });
 
 ipcMain.handle('getAllHistory', async function(event) {
-    const getAllAppointments = require('./db-management/Appointments/queries/getAllAppointments');
+    const getAllAppointments = require('./db-management/Get/getAllAppointments');
     return getAllAppointments(db);
 });
 
 ipcMain.handle('searchHistory', async function(event, searchString) {
-    const searchHistory = require('./db-management/Appointments/queries/searchHistory');
+    const searchHistory = require('./db-management/Search/searchHistory');
     return searchHistory(db, searchString);
 });
 
 ipcMain.handle('getUpcomingDropoffs', async function(event) {
-    const getUpcomingDropoffs = require('./db-management/Appointments/queries/getUpcomingDropoffs');
+    const getUpcomingDropoffs = require('./db-management/Get/getUpcomingDropoffs');
     return getUpcomingDropoffs(db);
 });
 
 ipcMain.handle('getUpcomingPickups', async function(event) {
-    const getUpcomingPickups = require('./db-management/Appointments/queries/getUpcomingPickups');
+    const getUpcomingPickups = require('./db-management/Get/getUpcomingPickups');
     return getUpcomingPickups(db);
 });
 
 ipcMain.handle('getCustomerBirds', async function(event, searchString) {
-    const getCustomerBirds = require('./db-management/Birds/queries/getCustomerBirds');
+    const getCustomerBirds = require('./db-management/Get/getCustomerBirds');
     return getCustomerBirds(db, searchString);
 });
 
