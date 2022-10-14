a971d96b7d5a9deaa7956a15459930c97c003a62
Kevin Stensberg <kstensberg@gmail.com>
2022-10-13 23:18:53 -0700

    fixes #69 add customer links to upcoming tables

--- src/renderer/components/upcoming-dropoffs-table.js
+++ src/renderer/components/upcoming-dropoffs-table.js
@@ -7,13 +7,29 @@ var UpcomingDropoffsTable = {
     oninit: async function(vnode) {
         const response = await window.contextBridge.database.getUpcomingDropoffs();
 
+        console.log(response);
+
         for (const row of response) {
-            this.dataRows.push([row.Date, row.Name, row.BirdName, row.BirdBreed, row.CageNeeded]);
+            this.dataRows.push([
+                row.Date, 
+                m('button', {
+                    'type': 'button',
+                    'class': 'btn btn-link',
+                    'onclick': async () => {
+                        await window.contextBridge.openCustomerDialog(row.CustomerId);
+                    }
+                }, row.Name), 
+                row.BirdName, 
+                row.BirdBreed, 
+                row.CageNeeded
+            ]);
         }
 
         m.redraw();
     },
     view: function(vnode) {
+
+        console.log(this.dataRows);
 
         return m(Table, {
             headers: ['Date', 'Customers', 'Bird Name', 'Bird Breed', 'Cage Needed'],
