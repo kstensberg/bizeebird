eff5cf56a49d493459ef2b06025ac2ad514f7e86
Kevin Stensberg <kstensberg@gmail.com>
2022-10-23 01:05:16 -0700

    fixes #65 add customerId to customer search

--- src/db-management/Search/searchCustomers.js
+++ src/db-management/Search/searchCustomers.js
@@ -4,7 +4,7 @@ const searchCustomers = (db, searchString) => {
     return new Promise((resolve, reject) => {
         db.serialize(() => {
             searchString = '%' + searchString + '%';
-            var query = 'SELECT Customers.Name AS name, CustomerPhoneNumbers.PhoneNumber AS phoneNumber, ' +
+            var query = 'SELECT Customers.CustomerId as customerId, Customers.Name AS name, CustomerPhoneNumbers.PhoneNumber AS phoneNumber, ' +
             'Customers.Email AS email, Customers.BoardingRate AS rate, Customers.Notes AS notes FROM Customers ' +
             'LEFT JOIN CustomerPhoneNumbers ON CustomerPhoneNumbers.Customer_CustomerId = Customers.CustomerId ' +
             'WHERE Customers.Email LIKE ? OR Customers.Name LIKE ? OR CustomerPhoneNumbers.PhoneNumber LIKE ? ' +
