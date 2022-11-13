04754f31b0b23d51027afc0f204c0938f5f20a9e
Shawn Stensberg <shawn.stensberg@gmail.com>
2022-11-13 14:32:20 -0800

    resolves #143 searchHistory syntax error

--- src/db-management/Search/searchHistory.js
+++ src/db-management/Search/searchHistory.js
@@ -7,7 +7,7 @@ const searchHistory = (db, searchString) => {
             var query = 'SELECT Customers.Name AS customerName, Customers.BoardingRate AS rate, Birds.Name AS birdName, Birds.Breed AS breed, ' +
             'Appointments.StartTime AS startDate, Appointments.EndTime AS endDate, ' +
             'Appointments.Status AS status, AppointmentBirds.GroomingWings AS wings, AppointmentBirds.GroomingNails AS nails, ' +
-            'AppointmentBirds.CageNeeded AS cage' +
+            'AppointmentBirds.CageNeeded AS cage ' +
             'FROM Appointments LEFT JOIN Customers ON Customers.CustomerId = Appointments.Customer_CustomerId LEFT JOIN AppointmentBirds ON ' +
             'AppointmentBirds.Appointment_AppointmentId = Appointments.AppointmentId LEFT JOIN Birds ON Birds.BirdId = AppointmentBirds.Bird_BirdId ' +
             'WHERE CustomerName LIKE ? OR BirdName LIKE ? OR Breed LIKE ? ORDER BY Appointments.StartTime DESC';
