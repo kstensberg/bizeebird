3e91e09c656e08c7c9844a2a4f3f1bac34c657b1
Kevin Stensberg <kstensberg@gmail.com>
2022-12-27 15:47:46 -0800

    upgrade dependencies

--- src/db-management/dbConfig.js
+++ src/db-management/dbConfig.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const dbLocation = process.env.APPDATA;
+const dbLocation = process.env.HOME;
 const db = new sqlite3.Database(dbLocation + '/BiZeeBird/bizeebird.db');
 
 module.exports = db;
