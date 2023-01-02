24c0cf69437853e01d080b32ae9059ea794997e2
Kevin Stensberg <kstensberg@gmail.com>
2023-01-02 12:13:46 -0800

    look for database in the right place

--- src/db-management/dbConfig.js
+++ src/db-management/dbConfig.js
@@ -1,5 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
-const dbLocation = process.env.HOME;
+
+const dbLocation = process.env.APPDATA;
 const db = new sqlite3.Database(dbLocation + '/BiZeeBird/bizeebird.db');
 
 module.exports = db;
