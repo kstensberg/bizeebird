525381e49284b3697ca8febca1bcc12650c10345
Shawn Stensberg <shawn.stensberg@gmail.com>
2023-03-25 02:41:49 -0700

    removed not null constraint from appt bird notes

--- src/db-management/Get/getAppointmentBirdNotes.js
+++ src/db-management/Get/getAppointmentBirdNotes.js
@@ -3,9 +3,9 @@
 const getAppointmentBirdNotes = (db, customerId) => {
     return new Promise((resolve, reject) => {
         db.serialize(() => {
-            db.all('SELECT ApptBirdNotes, AppointmentBirdId FROM AppointmentBirds ' +
+            db.all('SELECT ApptBirdNotes, AppointmentBirdId, CageNeeded FROM AppointmentBirds ' +
             'WHERE Bird_BirdId = (SELECT BirdId FROM Birds WHERE Customer_CustomerId = ?) ' +
-            'AND ApptBirdNotes IS NOT NULL ORDER BY AppointmentBirdId DESC LIMIT(1)', customerId, (err, rows) => {
+            'ORDER BY AppointmentBirdId DESC LIMIT(1)', customerId, (err, rows) => {
                 if (rows.length <= 0) {
                     resolve('');
                 } else {
